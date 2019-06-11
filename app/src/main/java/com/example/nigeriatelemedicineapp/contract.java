9c1929b45e465c85183ad5e6d9ab76fb976f9244
Rohan <rohan_dce@outlook.com>
2019-06-11 21:24:02 +0530

    added some of the views

--- app/src/main/java/com/example/nigeriatelemedicineapp/contract.java
+++
@@ -1,6 +0,0 @@
-package com.example.nigeriatelemedicineapp;
-
-public interface DashBoardcontract {
-
-       interface
-}
--- app/src/main/java/com/example/nigeriatelemedicineapp/presenter.java
+++
@@ -1,5 +0,0 @@
-package com.example.nigeriatelemedicineapp;
-
-public class presenter {
-
-}
