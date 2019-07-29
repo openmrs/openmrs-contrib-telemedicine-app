cf0846839aa3a3d0a23dee1931eef784b20524b3
Rohan <rohan_dce@outlook.com>
2019-07-29 23:39:49 +0530

    Added help Screen

---
+++ app/src/main/java/com/example/nigeriatelemedicineapp/registerpatient/Status.kt
@@ -0,0 +1,11 @@
+package com.example.nigeriatelemedicineapp.registerpatient
+
+enum class Status {
+    NETWORK_ERROR,
+    EMPTY_FIRST_NAME,
+    EMPTY_LAST_NAME,
+    EMPTY_GENDER,
+    EMPTY_DOB,
+    EMPTY_PHONE,
+    SUCCESS
+}
